833455a247b9c4d94bbca0b89c221ca3debae0de
Cate Ashley <catemarie.ashley@gmail.com>
2019-08-13 20:49:50 -0400

    setting up folder structures

---
+++ server.js
@@ -0,0 +1,24 @@
+// Dependencies
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+// Express app
+var app = express();
+var PORT = process.env.PORT;
+
+// Connect to CSS 
+app.use(express.static(path.join(__dirname, './app/public')));
+
+// Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+
+// Routes
+require(path.join(__dirname, './app/routing/apiRoutes'))(app);
+require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
+
+app.listen(PORT, function () {
+    console.log('Listening on PORT: ' + PORT);
+});
