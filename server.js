da67bad7db89a3f49cfa02a9d16a6482e8be306a
Cate Ashley <catemarie.ashley@gmail.com>
2019-08-13 23:52:25 -0400

    updated port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var path = require('path');
 
 // Express app
 var app = express();
-var PORT = process.env.PORT;
+var PORT = process.env.PORT || 8080;
 
 // Connect to CSS 
 app.use(express.static(path.join(__dirname, './app/public')));
